a7b9a428815cc751b6e841e77cf5faca09262e76
Neil Rackett <neil.rackett@mesmotronic.com>
2023-05-21 22:24:15 +0100

    Added docs script

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,13 @@ export * from './events/ConbineEvent';
 export * from './events/EventDispatcher';
 export * from './services/Actor';
 
+/**
+ * Ready-made Context instance for anyone who just wants a ready-to-go event
+ * bus without having to write any code
+ *
+ * @example
+ * import { ConbineEvent, eventBus } from 'conbine';
+ * eventBus.addEventListener('hello', event => console.log(event.data));
+ * eventBus.dispatchEvent(new ConbineEvent('hello', 'Hello, World!'))
+ */
 export const eventBus = new Context();
