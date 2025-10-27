33f8bc0ac35444e08ccf042aa2e341d9391b7021
Neil Rackett <neil.rackett@mesmotronic.com>
2025-10-27 09:49:54 +0000

    Exclude test folder from build

--- lib/decorators/Injectable.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Experimental class decorator to enable automatic property injection for
- * classes that implement the IInjectable interface
- *
- * @author	Neil Rackett
- */
-export declare function Injectable<T extends {
-    new (...args: any[]): {};
-}>(target: T): any;
