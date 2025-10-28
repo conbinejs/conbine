a4e47fe66d9d47c32fc4e623329143c8011a40bb
Neil Rackett <neil.rackett@mesmotronic.com>
2025-10-28 09:57:01 +0000

    Added tests for Actor

--- test/Context.test.ts
+++ test/Context.test.ts
@@ -10,7 +10,7 @@ describe("Context", () => {
     context = new Context();
   });
 
-  it("should map and call a command", () => {
+  it("should map, call and unmap a command", () => {
     const mockListener = vi.fn();
     class TestCommand extends Command<ConbineEvent> {
       async execute() {
@@ -20,25 +20,31 @@ describe("Context", () => {
     context.mapCommand("testEvent", TestCommand);
     context.dispatchEvent(new ConbineEvent("testEvent"));
     expect(mockListener).toHaveBeenCalled();
+    context.unmapCommand("testEvent", TestCommand);
+    expect(context.hasCommand("testEvent", TestCommand)).toBe(false);
   });
 
-  it("should map and inject a singleton", () => {
+  it("should map, inject and unmap a constant", () => {
+    const target: any = {};
+    context.mapConstant("testConstant", 123);
+    context.inject(target, "testConstant");
+    expect(target.testConstant).toBe(123);
+    context.unmapConstant("testConstant");
+    expect(context.hasConstant("testConstant")).toBe(false);
+  });
+
+  it("should map, inject and unmap a singleton", () => {
     class TestSingleton { }
 
     let target: any = {};
     context.mapSingleton("testSingleton", TestSingleton);
     context.inject(target, "testSingleton");
     expect(target.testSingleton).toBeInstanceOf(TestSingleton);
+    context.unmapSingleton("testSingleton");
+    expect(context.hasSingleton("testSingleton")).toBe(false);
   });
 
-  it("should map and inject a constant", () => {
-    const target: any = {};
-    context.mapConstant("testConstant", 123);
-    context.inject(target, "testConstant");
-    expect(target.testConstant).toBe(123);
-  });
-
-  it("should inject and uninject values", () => {
+  it("should inject and uninject multiple values", () => {
     context.mapConstant("foo", "bar");
     const target: any = {};
     context.inject(target, "foo");
